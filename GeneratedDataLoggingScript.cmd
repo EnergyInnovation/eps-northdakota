a7e8000da2c7c06e388c5c004ba4e5225a6b86f6
oashmoore <olivia@energyinnovation.org>
2023-05-10 15:13:20 -0700

    ira modeling

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -7,4 +7,10 @@ MENU>RUN|O
 MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2020|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
 
+SIMULATE>RUNNAME|IRA_Moderate_Incentives
+SIMULATE>READCIN|IRA_Moderate_Incentives.cin
+MENU>RUN|O
+MENU>VDF2TAB|IRA_Moderate_Incentives.vdfx|IRA_Moderate_Incentives.tsv|OutputVarsToExport.lst|+!||2020|2050|:IRA_Moderate_Incentives
+FILE>DELETE|IRA_Moderate_Incentives.vdfx
+
 SIMULATE>SAVELIST|
